ad6976874b077e02f7542e5be574082b727c882b
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2023-06-05 22:06:22 +0300

    B6 - Session API

--- src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
+++ src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
@@ -20,13 +20,18 @@ public class DemostoreApiSimulation extends Simulation {
     Map.entry("authorization", "Bearer #{jwt}")
   );
 
+  private static ChainBuilder initSession = exec(session -> session.set("authenticated", false));
+
   private static class Authentication {
 
-    private static ChainBuilder authenticate = exec(http("Authenticate")
-            .post("/api/authenticate")
-            .body(StringBody("{\"username\": \"admin\",\"password\": \"admin\"}"))
-            .check(status().is(200))
-            .check(jmesPath("token").saveAs("jwt")));
+    private static ChainBuilder authenticate =
+            doIf(session -> !session.getBoolean("authenticated")).then(
+                exec(http("Authenticate")
+                    .post("/api/authenticate")
+                    .body(StringBody("{\"username\": \"admin\",\"password\": \"admin\"}"))
+                    .check(status().is(200))
+                    .check(jmesPath("token").saveAs("jwt")))
+                    .exec(session -> session.set("authenticated", true)));
   }
 
   private static class Categories {
@@ -71,6 +76,7 @@ public class DemostoreApiSimulation extends Simulation {
   }
 
   private ScenarioBuilder scn = scenario("DemostoreApiSimulation")
+    .exec(initSession)
     .exec(Categories.list)
     .pause(86)
     .exec(Products.list)
