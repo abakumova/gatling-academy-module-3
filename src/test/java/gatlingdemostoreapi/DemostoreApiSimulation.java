24e539c531f96a8710043598e824dcf8fe8eced8
Viktoriia Abakumova <abakumovaviktory@gmail.com>
2023-06-13 14:55:45 +0300

    C7 - Runtime Parameters

--- src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
+++ src/test/java/gatlingdemostoreapi/DemostoreApiSimulation.java
@@ -21,6 +21,14 @@ public class DemostoreApiSimulation extends Simulation {
     Map.entry("authorization", "Bearer #{jwt}")
   );
 
+  private static final int USER_COUNT = Integer.parseInt(System.getProperty("USERS", "5"));
+
+  private static final Duration RAMP_DURATION =
+            Duration.ofSeconds(Integer.parseInt(System.getProperty("RAMP_DURATION", "10")));
+
+  private static final Duration TEST_DURATION =
+            Duration.ofSeconds(Integer.parseInt(System.getProperty("DURATION", "60")));
+
   private static ChainBuilder initSession = exec(session -> session.set("authenticated", false));
 
   private static class Authentication {
@@ -147,7 +155,7 @@ public class DemostoreApiSimulation extends Simulation {
     private static class Scenarios {
 
         public static ScenarioBuilder defaultScn = scenario("Default load test")
-                .during(Duration.ofSeconds(60))
+                .during(TEST_DURATION)
                 .on(
                         randomSwitch().on(
                                 Choice.withWeight(20d, exec(UserJourneys.admin)),
@@ -157,7 +165,7 @@ public class DemostoreApiSimulation extends Simulation {
                 );
 
         public static ScenarioBuilder noAdminsScn = scenario("Load test without admin users")
-                .during(Duration.ofSeconds(60))
+                .during(TEST_DURATION)
                 .on(
                         randomSwitch().on(
                                 Choice.withWeight(60d, exec(UserJourneys.priceScrapper)),
@@ -182,13 +190,18 @@ public class DemostoreApiSimulation extends Simulation {
 //                    rampConcurrentUsers(1).to(5).during(Duration.ofSeconds(20)),
 //                    constantConcurrentUsers(5).during(Duration.ofSeconds(20))))
 //            .protocols(httpProtocol);
-            Scenarios.defaultScn.injectOpen(constantUsersPerSec(2).during(Duration.ofMinutes(3)))
-                    .protocols(httpProtocol)
-                    .throttle(
-                            reachRps(10).in(Duration.ofSeconds(30)),
-                            holdFor(Duration.ofSeconds(60)),
-                            jumpToRps(20),
-                            holdFor(Duration.ofSeconds(60))))
-            .maxDuration(Duration.ofMinutes(3));
+
+//            Scenarios.defaultScn.injectOpen(constantUsersPerSec(2).during(Duration.ofMinutes(3)))
+//                    .protocols(httpProtocol)
+//                    .throttle(
+//                            reachRps(10).in(Duration.ofSeconds(30)),
+//                            holdFor(Duration.ofSeconds(60)),
+//                            jumpToRps(20),
+//                            holdFor(Duration.ofSeconds(60))))
+//            .maxDuration(Duration.ofMinutes(3));
+
+            Scenarios.defaultScn
+                    .injectOpen(rampUsers(USER_COUNT).during(RAMP_DURATION))
+                    .protocols(httpProtocol));
   }
 }
